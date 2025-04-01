0e5e64cb70e764ff42412edc9724b828c659fe57
이본우 <bw.lee@okestro.com>
2025-04-01 13:03:36 +0900

    UnitByteToByte 함수 추가

--- butil/function_size.go
+++ butil/function_size.go
@@ -1,10 +1,13 @@
 package butil
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
 // Kibibytes to Kilobytes
-//	- Kilobytes = Kibibytes x 1024 ÷ 1000
-//	- Kilobytes = Kibibytes x 1.024
+//   - Kilobytes = Kibibytes x 1024 ÷ 1000
+//   - Kilobytes = Kibibytes x 1.024
 func KiByteToKByte(kibibytes int) int {
 	return int((float64(kibibytes) * 1.024) / 1)
 }
@@ -14,8 +17,8 @@ func KiByteToKByteStr(kibibytes int) string {
 }
 
 // Mebibytes to Megabytes
-//	- Megabytes = Mebibytes x (1024x1024) ÷ (1000x1000)
-//	- Megabytes = Mebibytes x 1.048576
+//   - Megabytes = Mebibytes x (1024x1024) ÷ (1000x1000)
+//   - Megabytes = Mebibytes x 1.048576
 func MiByteToMByte(mebibyte int) int {
 	return int((float64(mebibyte) * 1.048576) / 1)
 }
@@ -25,12 +28,32 @@ func MiByteToMByteStr(mebibyte int) string {
 }
 
 // Gibibytes to Gigabytes
-//	- Gigabytes = Gibibytes x (1024x1024x1024) ÷ (1000x1000x1000)
-//	- Gigabytes = Gibibytes x 1.073741824
+//   - Gigabytes = Gibibytes x (1024x1024x1024) ÷ (1000x1000x1000)
+//   - Gigabytes = Gibibytes x 1.073741824
 func GiByteToGByte(gibibyte int) int {
 	return int((float64(gibibyte) * 1.073741824) / 1)
 }
 
 func GiByteToGByteStr(gibibyte int) string {
 	return strconv.Itoa(GiByteToGByte(gibibyte))
+}
+
+// 함수 내에서 unit 을 ToLower + TrimSpace 처리함
+// unit : kb, mb, gb, tb
+func UnitByteToByte(val uint64, unit string) uint64 {
+	unit = strings.ToLower(unit)
+	unit = strings.TrimSpace(unit)
+	const mul uint64 = 1024
+
+	if unit == "kb" {
+		return val * mul
+	} else if unit == "mb" {
+		return val * mul * mul
+	} else if unit == "gb" {
+		return val * mul * mul * mul
+	} else if unit == "tb" {
+		return val * mul * mul * mul * mul
+	}
+
+	return 0
 }

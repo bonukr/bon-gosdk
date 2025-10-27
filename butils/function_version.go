0c86005855f6cf8216b881350138535f293d2e2c
이본우 <bw.lee@okestro.com>
2025-10-27 14:40:52 +0900

    .

--- butils/function_version.go
+++ butils/function_version.go
@@ -16,12 +16,12 @@ func ParseVersion(ver string) (major int, minor int, patch int) {
 		}
 	}
 	if tmp >= 2 {
-		if major, err = strconv.Atoi(strings.TrimSpace(parts[1])); err != nil {
+		if minor, err = strconv.Atoi(strings.TrimSpace(parts[1])); err != nil {
 			minor = -1
 		}
 	}
 	if tmp >= 3 {
-		if major, err = strconv.Atoi(strings.TrimSpace(parts[2])); err != nil {
+		if patch, err = strconv.Atoi(strings.TrimSpace(parts[2])); err != nil {
 			patch = -1
 		}
 	}

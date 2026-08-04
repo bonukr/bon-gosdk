abe0855a50bc7e21b7fa208512de5a71d0520f50
이본우 <bw.lee@okestro.com>
2026-08-04 12:07:57 +0900

    Add TruncateWithEllipsis

--- butils/function_string.go
+++ butils/function_string.go
@@ -2,10 +2,19 @@ package butils
 
 import (
 	"strings"
+	"unicode/utf8"
 )
 
 func RemoveNewlines(s string) string {
 	s = strings.ReplaceAll(s, "\r\n", "")
 	s = strings.ReplaceAll(s, "\n", "")
 	return s
+}
+
+func TruncateWithEllipsis(s string, limit int) string {
+	if utf8.RuneCountInString(s) <= limit {
+		return s
+	}
+	runes := []rune(s)
+	return string(runes[:limit]) + "..."
 }
